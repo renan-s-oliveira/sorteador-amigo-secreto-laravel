6ab21c7450e7568523ecf77e35ae5ecc33216136
LG560 <enapoli7@gmail.com>
2021-11-27 13:45:37 -0300

    fix campo cep migration

--- database/migrations/2021_11_27_135443_add_novos_campos_to_membro_sorteios_table.php
+++ database/migrations/2021_11_27_135443_add_novos_campos_to_membro_sorteios_table.php
@@ -19,7 +19,7 @@ class AddNovosCamposToMembroSorteiosTable extends Migration
             $table->string('complemento')->nullable();
             $table->string('bairro')->nullable();
             $table->string('cidade')->nullable();
-            $table->string('cep ')->nullable();
+            $table->string('cep')->nullable();
         });
     }
 
